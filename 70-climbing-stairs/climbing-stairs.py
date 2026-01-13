a42b9094974a0915372da0c5500ed02e44e531f1
HashimTaha <137400077+HashimTaha30@users.noreply.github.com>
2026-01-13 18:43:18 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (6.10%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,16 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+
+        a = 1  # ways to reach step 1
+        b = 2  # ways to reach step 2
+
+        for _ in range(3, n + 1):
+            c = a + b
+            a = b
+            b = c
+
+        return b
